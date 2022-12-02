ee678969537b0fba283b371b973ec45d5ac49199
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-02 14:47:21 -0500

    created template and psuedocode for thought controller.

---
+++ controllers/thought-controller.js
@@ -0,0 +1,11 @@
+const { Thought } = require('../models/Thought');
+
+const thoughtController = {
+// GET to get all thoughts
+// GET to get a single thought by its _id
+// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+// PUT to update a thought by its _id
+// DELETE to remove a thought by its _id
+};
+
+module.exports = thoughtController;
