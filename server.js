c87be33f5692ba16d66cf8cb6db5f3f6188ad35d
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-02 11:57:20 -0500

    established connection to mongo db and created a link for logged local host location.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // require express, database connection and routes
 const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 // restablish PORT and store express in app to utilize 
 const PORT = 3001;
@@ -10,11 +10,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 // open db in order to listen on port
 db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
+        console.log(`Example app listening at http://localhost:${PORT}`);
     });
   });
