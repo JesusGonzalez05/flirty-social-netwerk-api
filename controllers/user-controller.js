46dc83ffdc457e35db9ff3872b102702bf903e2a
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-02 14:43:51 -0500

    created template and psuedocode for user controller.

---
+++ controllers/user-controller.js
@@ -0,0 +1,11 @@
+const { User } = require('../models/User');
+
+const userController = {
+// get all users
+// GET a single user by its _id and populated thought and friend data
+// POST a new user
+// PUT to update a user by its _id
+// DELETE to remove user by its _id
+};
+
+module.exports = userController;
